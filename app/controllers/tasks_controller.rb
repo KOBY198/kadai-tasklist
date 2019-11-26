7aa4ecacf199503719ac6101e67cefda0d03321b
KOBY198 <hi.koby117@gmail.com>
2019-11-26 23:55:14 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
       @tasks = Task.all
   end
 
   def show
-      @tasks = Task.find(params[:id])
   end
 
   def new
@@ -23,11 +23,9 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @tasks = Task.find(params[:id])
   end
 
   def update
-      @tasks = Task.find(params[:id])
 
     if @tasks.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -39,15 +37,18 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @tasks = Task.find(params[:id])
     @tasks.destroy
 
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
   end    
+
+  def set_task
+    @tasks = Task.find(params[:id])
+  end
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
 end
