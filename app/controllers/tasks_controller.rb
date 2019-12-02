67a9c352d43d71ab9935c49e3d530458a7721dfc
KOBY198 <hi.koby117@gmail.com>
2019-12-02 13:50:43 +0000

    kadai tasklist login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,17 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+ # before_action :require_user_logged_in
+ # before_action :correct_user, 
+
   def index
-      @tasks = Task.order(id: :desc).page(params[:page]).per(5)
+    if logged_in?
+      #@tasks = Task.order(id: :desc).page(params[:page]).per(5)
+      #@tasks = Task.where(user_id: session[:user_id]).order(id: :desc).page(params[:page]).per(5)
+      @user = User.find_by(id: session[:user_id])
+      @tasks = @user.tasks.order(id: :desc).page(params[:page]).per(5)
+    else
+      redirect_to login_path
+    end
   end
 
   def show
@@ -12,11 +22,13 @@ class TasksController < ApplicationController
   end
 
   def create
-      @tasks = Task.new(task_params)
+      @tasks = current_user.tasks.build(task_params)
+      #@tasks = Task.new(task_params)
       if @tasks.save
         flash[:success] = 'Task が正常に投稿されました'
-        redirect_to @tasks
+        redirect_to tasks_url
       else
+#        @tasks = current_user.tasks.order(id: :desc).page(params[:page])
         flash.now[:danger] = 'Task が投稿されませんでした'
         render :new
       end
@@ -26,10 +38,9 @@ class TasksController < ApplicationController
   end
 
   def update
-
     if @tasks.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @tasks
+      redirect_to tasks_url
     else
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
@@ -48,7 +59,14 @@ class TasksController < ApplicationController
   end
   
   def task_params
+    #params.require(:task).permit(:content, :status, :user)
     params.require(:task).permit(:content, :status)
   end
   
+#  def correct_user
+#    @task = current_user.tasks.find_by(id: params[:id])
+#    unless @task
+#      redirect_to root_url
+#    end
+#  end
 end
